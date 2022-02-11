9b3b29cc38b5f4fde6411ff0ba8441780cb11035
sophiabevans <sophiajbevans@gmail.com>
2022-02-10 23:20:32 -0500

    rosalind problem 4 solution, submitted output 108412748857 to rosalind.info

---
+++ rosalind-4/rosalind-4.py
@@ -0,0 +1,22 @@
+import sys
+
+file_name = sys.argv[1]
+
+with open(file_name, "r") as f:
+    line = f.readline()
+    n, k = line.strip().split()
+
+months = int(n)
+litter_size = int(k)
+
+def rosalind4(months, litter_size):
+    rabbits = [1, 1] #F0 = F1 = 1
+    i = 1 #index variable
+    while len(rabbits) < months:
+        #offspring = rabbits alive months ago * k, add new rabbits
+        rabbits.append((rabbits[i-1] * litter_size) + rabbits[i])
+        i += 1 #increment index
+    return rabbits[-1]
+
+if __name__ == "__main__":
+    print(rosalind4(months, litter_size))
---
+++ rosalind_soln_template.py
@@ -0,0 +1,12 @@
+import sys
+
+file_name = sys.argv[1]
+
+def rosalind():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    rosalind(file_name)
